f0e4df56ade13c1fed731b8663b6f205fa45cbaf
Travis van der Font <travis.font@gmail.com>
2015-05-13 14:25:26 +0200

    --

---
+++ library/TypeOf.js
@@ -0,0 +1,20 @@
+var TypeOf = {
+
+    isFunction: function(obj)
+    {
+        if (typeof obj === 'function')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
+
+
+// Example:
+if (TypeOf.isFunction(windows['test'])) {
+    // ....
+}
