5db616c03b0355d670e36ce43f9a7b1d4f6dd850
Travis van der Font <travis.font@gmail.com>
2015-09-09 10:34:09 +0200

    -

--- library/ExtMath.js
+++ library/ExtMath.js
@@ -1,13 +1,32 @@
-var TypeOf = {
+// extended math library
+
+var ExtMath = {
+
+    getDegToRad: function(deg)
+    {
+        return deg * (Math.PI / 180);
+    }
+    
+    // Haversine formula - calculates the distance between two points (coordinates)
+    getGreatCircleDistance: function(lat1, lon1, lat2, lon2)
+    {
+        var p = 0.017453292519943295; // Math.PI / 180
+        var c = Math.cos;
+        var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+                c(lat1 * p) * c(lat2 * p) * 
+                (1 - c((lon2 - lon1) * p))/2;
+
+        return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+    }
 
     random: function (maxRange)
     {
-        Math.floor((Math.random() * maxRange) + 1);
+        return Math.floor((Math.random() * maxRange) + 1);
     }
 
     randomEvenOnly: function (maxRange)
     {
-        return Math.floor( Math.random() * maxRange / 2 ) * 2;
+        return Math.floor(Math.random() * maxRange / 2) * 2;
     }
 
 };
