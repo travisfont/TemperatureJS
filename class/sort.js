07245a20c4dc5a5758a84444be26691ece749c80
Travis van der Font <travis.font@gmail.com>
2016-03-27 19:09:18 +0200

    add Quick Sorting to sort class

--- class/sort.js
+++ class/sort.js
@@ -44,6 +44,33 @@ var sort =
         return array;
     },
     
+    // Quick Sorting
+    quick function (array)
+    {
+        if (array.length < 2)
+        {
+            return array;
+        }
+
+        var pivot   = array[0];
+        var lesser  = [];
+        var greater = [];
+
+        for (var i = 1; i < array.length; i++)
+        {
+            if (array[i] < pivot)
+            {
+                lesser.push(array[i]);
+            }
+            else
+            {
+                greater.push(array[i]);
+            }
+        }
+
+        return quicksortBasic(lesser).concat(pivot, quicksortBasic(greater));
+    }
+    
     _gaps = [701, 301, 132, 57, 23, 10, 4, 1],
     
     _swap: function (array, i, j)
