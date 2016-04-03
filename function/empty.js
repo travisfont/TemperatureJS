e00d886f114a9ed2952bed12180e3885019ca2f9
Travis van der Font <travis.font@gmail.com>
2016-04-03 22:31:59 +0200

    minor strict condition update to empty()

--- function/empty.js
+++ function/empty.js
@@ -1,6 +1,6 @@
 function empty(str)
 {
-    if (!str || str.length === 0 || str === "" || typeof str == 'undefined' || !/[^\s]/.test(str) || /^\s*$/.test(str) || str.replace(/\s/g,"") == "")
+    if (!str || str.length === 0 || str === "" || typeof str == 'undefined' || !/[^\s]/.test(str) || /^\s*$/.test(str) || str.replace(/\s/g,"") === "")
     {
         return true;
     }
