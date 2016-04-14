efeeab0c1c79255dbcdfd41424da48fce2f37656
Travis van der Font <travis.font@gmail.com>
2016-04-14 19:33:24 +0200

    add call_browser_notification() to support native browser notifications

---
+++ function/call_browser_notification.js
@@ -0,0 +1,40 @@
+function call_browser_notification(title, icon description, url, onClick, onClose) 
+{
+        if (!Notification)
+        {
+           console.log('Desktop notifications not supported in your browser..'); 
+           return;
+         }
+         
+        // Grant permission from user 
+        if (Notification.permission !== 'granted')
+        {
+            Notification.requestPermission();
+        }
+        else
+        {
+            var notification = new Notification(title,
+            {
+                icon: icon,
+                body: description,
+            });
+ 
+        // Remove the notification from Notification Center when clicked.
+        notification.onclick = function ()
+        {
+            if (url.length > 0)
+            {
+                if (/^(http|https):\/\/[^ "]+$/.test(url) === TRUE)
+                {
+                    window.open(url);
+                }
+            }
+        };
+ 
+        // Callback function when the notification is closed.
+        notification.onclose = function ()
+        {
+            console.log('Notification closed');
+        };
+     }
+}
