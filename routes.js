5db25a0ff8c74939c11405d74d618a30150986c5
Travis van der Font <travis.font@gmail.com>
2016-04-30 21:57:08 +0200

    routes are ALIVE!

--- routes.js
+++ routes.js
@@ -1,19 +1,12 @@
-router('/test', function()
+route('/', function ()
+{
+    alert('welcome!');
+});
+
+route('/test', 'template1', function ()
 {
     document.getElementById('targetText').innerHTML +="<div>Welcome to the test page!</div>";
     console.log('This is a functional test call!');
 });
 
-router = function (uri, callback)
-{
-    var regex = new RegExp(uri + '$');
-    var match = regex.exec(location.pathname);
-
-    if (match)
-    {
-        document.addEventListener('DOMContentLoaded', function()
-        {
-            callback.apply(callback, match.splice(1));
-        });
-    }
-};
+// https://gist.github.com/joakimbeng/7918297
