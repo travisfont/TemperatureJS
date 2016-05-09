d4c2ef9f7a6ab7d05c0f97aa96fbd29759df3b59
Travis van der Font <travis.font@gmail.com>
2016-05-09 16:45:36 +0200

    -

--- initizer.js
+++ initizer.js
@@ -12,62 +12,9 @@ var bootloaders =
     //LIBRARY_PATH + 'plugins/crypto-js/enc-base64-min.js'  // CryptoJS 3.1
 ];
 
-function setHeaderCSS(inline_css)
-{
-    css    = [];
-    css[0] = inline_css;
-
-    var headTag = document.getElementsByTagName('head')[0],
-        el = document.createElement('style');
-
-    el.type = 'text/css';
-    el.textContent = css.join('\n');
-
-    headTag.appendChild(el);
-}
 
 //---
 
-function setTitle(title)
-{
-    document.title = title;
-}
-
-function setHTML(content)
-{
-    if (document.body.innerHTML.length == 0)
-    {
-        document.body.innerHTML = content;
-    }
-    else
-    {
-        document.body.innerHTML += content;
-    }
-}
-
-function createObject(type, text)
-{
-    if (type == 'div')
-    {
-        var div = document.createElement("div");
-        div.style.width = "100px";
-        div.style.height = "100px";
-        div.style.background = "red";
-        div.style.color = "white";
-        div.innerHTML = text;
-
-        document.body.appendChild(div);
-    }
-}
-
-function createButton(text)
-{
-    var btn = document.createElement("BUTTON");  // Create a <button> element
-    var t = document.createTextNode(text);       // Create a text node
-
-    btn.appendChild(t);                         // Append the text to <button>
-    document.body.appendChild(btn);
-}
 function createImage(){}
 function createLink(){} // createLink().removeUnderline()
 function createSubmitButton(){}
