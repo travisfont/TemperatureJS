fc5728651dff48c04a57d7892cd0afe7f59a0c1e
Travis van der Font <travis.font@gmail.com>
2016-06-17 17:20:28 +0200

    Code update: Checking for undefined would need to be moved to first in the checks, or undefined items will throw exceptions on the earlier checks.

--- function/empty.js
+++ function/empty.js
@@ -1,6 +1,6 @@
 function empty(str)
 {
-    if (!str || str.length === 0 || str === "" || typeof str == 'undefined' || !/[^\s]/.test(str) || /^\s*$/.test(str) || str.replace(/\s/g,"") === "")
+    if (typeof str == 'undefined' || !str || str.length === 0 || str === "" || !/[^\s]/.test(str) || /^\s*$/.test(str) || str.replace(/\s/g,"") === "")
     {
         return true;
     }
