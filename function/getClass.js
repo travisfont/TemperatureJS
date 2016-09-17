b8ccd933fcc3e4cbae04bdce58bfed616b1c8a17
Travis van der Font <travis.font@gmail.com>
2016-09-17 14:58:50 +0200

    update to getClass()

--- function/getClass.js
+++ function/getClass.js
@@ -1,6 +1,8 @@
 // get elements by class name
 
-function getClass(name)
+function getClass(name, node)
 {
+    // if node !== null && node >= 0 (is integer)
+    // return document.getElementsByClassName(name)[node];
     return document.getElementsByClassName(name);
 }
